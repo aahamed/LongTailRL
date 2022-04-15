c0eed6fbedce43bd187b814633fcf1cbb550835f
aadil <aadil.ahamed@gmail.com>
2022-04-15 01:57:48 -0700

    add option to skip training

--- ppo_exp.py
+++ ppo_exp.py
@@ -27,11 +27,13 @@ def main( args ):
     # instantiate agent
     model = PPO(args.policy, env, verbose=1)
     model.set_logger(new_logger)
-    # train the agent
-    model.learn(total_timesteps=int(args.n_steps))
-    # save the agent
+
     model_path = os.path.join(exp_path, "model")
-    model.save(model_path)
+    if not args.skip_train:
+        # train the agent
+        model.learn(total_timesteps=int(args.n_steps))
+        # save the agent
+        model.save(model_path)
 
     # load the trained agent
     model = PPO.load(model_path, env=env)
@@ -40,7 +42,7 @@ def main( args ):
 
     # Evaluate the agent
     eps_returns, _ = evaluate_policy(model, model.get_env(),
-            n_eval_episodes=100, return_episode_rewards=True)
+            n_eval_episodes=1000, return_episode_rewards=True)
 
     eps_returns = np.array( eps_returns )
     print(f"mean return: {eps_returns.mean():.2f} " +
@@ -65,5 +67,7 @@ if __name__ == "__main__":
             help="environment id")
     parser.add_argument("--policy", type=str, default="MlpPolicy",
             help="policy architecture")
+    parser.add_argument("--skip-train", action="store_true",
+            help="skip training")
     args = parser.parse_args()
     main( args )
